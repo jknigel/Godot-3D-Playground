9391650ff7d9b9865a1c64b517b4b75386274ad5
Jknigel <ajknigel360@gmail.com>
2025-11-08 00:47:26 +0800

    Release 1.0.0

--- player.gd
+++ player.gd
@@ -27,6 +27,9 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(direction)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
@@ -55,7 +58,10 @@ func _physics_process(delta):
 				target_velocity.y = bounce_impulse
 				break
 	
+	velocity = target_velocity
 	move_and_slide()
+	
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 	
 	
 func die():
